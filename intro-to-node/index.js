0e56cab05068d14da3f54f0177044e477532fb8e
lucas <lucas.lambda.101@gmail.com>
2021-03-26 16:16:48 -0300

    Finish Intro to Node project

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySupervillainName);
