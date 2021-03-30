f1da9a1b5766aa14a2b74000e98a71c8ba68deb7
lucas <lucas.lambda.101@gmail.com>
2021-03-30 16:37:17 -0300

    Add input city

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -2,11 +2,18 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-  const query = "Tokio";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
   const apiKey = "bbd0a8917f02a462b67a5927e3f1529d";
   const unit = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`;
@@ -22,7 +29,7 @@ app.get("/", function(req, res) {
       const imgUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
 
       res.write(`<p>The weather is currently ${desc}.</p>`);
-      res.write(`<h1>The temperature in Tokio is ${temp} degrees Celsius.</h1>`);
+      res.write(`<h1>The temperature in ${query} is ${temp} degrees Celsius.</h1>`);
       res.write(`<img src="${imgUrl}" />`)
       res.send();
     });
