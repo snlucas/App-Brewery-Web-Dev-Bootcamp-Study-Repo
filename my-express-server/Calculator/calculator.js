b6878ac2ece538ed9af13fc36fa98569ede7e4a3
lucas <lucas.lambda.101@gmail.com>
2021-03-29 21:59:53 -0300

    Add a sum method

--- my-express-server/Calculator/calculator.js
+++ my-express-server/Calculator/calculator.js
@@ -1,11 +1,23 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", function(req, res) {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  const sum = num1 + num2;
+
+  res.send(sum.toString());
 });
 
 app.listen(3000, () => console.log("Hey ya!"));
